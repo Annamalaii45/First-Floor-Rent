92bff1ad68f13b6a5e8206ed7761e3c2c4912a2f
Annamalaii45 <100117262+Annamalaii45@users.noreply.github.com>
2022-02-23 23:06:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+house="""                                      First Floor Rent
+                                      House owner: Annamalaii
+                                       Mobile no: 123456789"""
+
+
+print(house)
+
+rent=45000
+if_he_pays_perfectly=False
+
+
+if if_he_pays_perfectly:
+    rent = rent * 12
+
+else:
+    print("Sorry You Are Not Eligible")
+    print()
+
+print(f"Payment For A Year(12 Months):{rent}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
